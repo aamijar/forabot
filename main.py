75a92be0ba4a2afbafb5fca728643ae06a91adcc
Anupam Mijar <aamijar230@gmail.com>
2022-04-22 11:38:21 -0400

    add data and upload

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from keras.applications import resnet
 from keras.applications import vgg16
 from keras.applications import xception
 
+
+img_path = input('Enter image path: ')
+
 img_shape = (224, 224)
 labels = ['G. Bulloides', 'G. Ruber', 'G. Sacculifer', 'N. Dutertrei', 'N. Incompta', 'N. Pachyderma', 'Others']
 
@@ -14,14 +17,16 @@ model = load_model("./models/foram.h5")
 xception_model = xception.Xception(include_top=False, pooling='avg')
 resnet50_model = resnet.ResNet50(include_top=False, pooling='avg')
 
-img_path = "./data//02-24-17_Trial_1 G. Bulloides OOP 552A 7-9 cm 250-355 micro/"
-img_path = "./data/02-24-17_Trial_2 G. Bulloides OOP 552A 7-9 cm 250-355 micro/"
-img_path = "./data/02-24-17_Trial_3 G. Bulloides OOP 552A 7-9 cm 250-355 micro/"
+# img_path = "./data/02-24-17_Trial_1 G. Bulloides OOP 552A 7-9 cm 250-355 micro/"
+# img_path = "./data/02-24-17_Trial_2 G. Bulloides OOP 552A 7-9 cm 250-355 micro/"
+# img_path = "./data/02-24-17_Trial_3 G. Bulloides OOP 552A 7-9 cm 250-355 micro/"
 img_filenames = [img_path + p for p in os.listdir(img_path)]
+
 
 group_images = np.zeros(img_shape + (len(img_filenames),))
 for i, img_file in enumerate(img_filenames):
     img = cv2.imread(img_file, 0)
+    saveimg = cv2.imread(img_file)
     img = cv2.resize(img, img_shape, interpolation=cv2.INTER_CUBIC)
     group_images[:, :, i] = img
 img90 = np.expand_dims(np.percentile(group_images, 90, axis=-1), axis=-1)
@@ -37,6 +42,19 @@ feats = []
 feats.append(fea)
 arr = np.squeeze(fea)
 
+# print(fea)
+# print(fea.shape)
 classes = model.predict(fea)
 print("Confidence:", np.max(classes))
 print("Label:", labels[np.argmax(classes)])
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(saveimg, labels[np.argmax(classes)] + " " + str(np.max(classes)), (20,50), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+cv2.imwrite("./data/" + 'pred.jpg', saveimg)
+
+
+from upload import upload_blob
+
+
+img_path = os.path.basename(os.path.normpath(img_path))
+upload_blob("forabot-web", "./data/pred.jpg", f"pred-{img_path}.jpg")
